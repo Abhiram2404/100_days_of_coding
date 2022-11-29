d33886f6f97f088ad1c51a6fb9fb2f08f65b7e6c
ram <abhiram9346616736@gmail.com>
2022-11-29 23:02:14 +0530

    My first Day code Day1

---
+++ Day1.py
@@ -0,0 +1,8 @@
+print("Abhiram")
+print("November 24th 2022")
+print("i'm signing up for replit's 100 days of python challange!")
+print(
+    """i'll make sure to spend some time every day on coading along, for minimum 10 minutes a day ill be using repklit an amazing online IDE so i can do this from my phone whenever i happen to be. No excuses for not coding from the middle of a field"""
+)
+print(" I'm feeling great")
+print("you can follow my progress at ABHIRAM KULAKARN")
