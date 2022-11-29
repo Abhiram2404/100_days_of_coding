5cf43797b621e4652ccb4b43ea1a9d22c1410189
ram <abhiram9346616736@gmail.com>
2022-11-29 23:03:25 +0530

    My Third Day code Day3

---
+++ Day3.py
@@ -0,0 +1,9 @@
+food = input("Enter food you want: ")  #curry  #inputs_for_code
+plant = input("What plant should be used: ")  #Ladyfinger
+method_to_cook = input("mention method of cooking: ")  #hot
+Describe = input("describe how u want the food: ")  #fry
+item= input("name the item: ")  #onion
+print()
+print("Today make",food,"with",plant,"and it should be",method_to_cook,"and make it",Describe,"and use",item,"in it")
+print()
+#output : Today make curry with ladies finger and it should be hot and make it fry and use onion in it.
