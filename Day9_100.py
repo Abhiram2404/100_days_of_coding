aa2a5ed70dc46d55a7ec89c241f6aae5328ad30b
Abhiram <abhiram9346616736@gmail.com>
2022-12-04 17:25:57 +0530

    This is my 9th day of code

---
+++ Day9_100.py
@@ -0,0 +1,11 @@
+Year = int(input("born date- "))
+if Year <=1946:
+  print("you r from Traditionalists Generation")
+elif Year <= 1964:
+  print("you r from Baby Boomers Generation")
+elif Year <= 1981:
+  print("you r from Generation X Generation")
+elif Year <=1995:
+  print("you r from Millenials Generation")
+else:
+  print("you r from Generation Z Generation")
